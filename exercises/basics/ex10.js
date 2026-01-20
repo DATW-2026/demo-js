45db48c32b5a28dc97da90f07bbfe4c913b97fe0
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-01-20 14:41:44 +0100

    Add exercise 10

---
+++ exercises/basics/ex10.js
@@ -0,0 +1,7 @@
+// 10. Crea una función que imprima por consola un número al azar entre 0 y 10.
+
+function randomNum(max) {
+    return Math.floor(Math.random() * max);
+}
+
+console.log(randomNum(11));
