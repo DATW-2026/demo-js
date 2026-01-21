bd9ab6126fba1f1c3f40a5897f078e74ec4c4442
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-01-21 11:19:20 +0100

    Update exercise 7 with functional expression and arrow functions

--- exercises/basics/ex7.js
+++ exercises/basics/ex7.js
@@ -3,11 +3,23 @@
 const numA = 5;
 const numB = 10;
 
+// Declaración de función
+
 function mult(a, b) {
     return a * b;
 }
 
 console.log(mult(numA, numB)); // 50
 
+// Asignación de una expresión funcional:
+
+const multiply = function (a, b) {
+    return a * b;
+};
+
+console.log(multiply(numA, numB));
+
+// Arrow function o función flecha
 const arrowMult = (a, b) => a * b;
+
 console.log(arrowMult(numA, numB)); // 50
