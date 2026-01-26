29d67395a8bbcefb0e4fa13614ea0c9542102670
Yevestevez <yevestevez.cg@gmail.com>
2026-01-26 19:38:24 +0100

    Add exercise 4 medium

---
+++ exercises/medium/ex4.js
@@ -0,0 +1,24 @@
+// Crea una función que verifique si una cadena de texto recibida por parámetros es un pangrama (contiene todas las letras del abecedario).
+
+const text = 'abcdefghijklmnñopqrstuvwxyz';
+
+{
+    const isPangram = (text) => {
+        const alphabet = 'abcdefghijklmnñopqrstuvwxyz';
+        const normalized = text.toLowerCase();
+
+        for (let i = 0; i < alphabet.length; i++) {
+            if (!normalized.includes(alphabet[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    console.log(
+        isPangram(text)
+            ? `${text} es un pangrama`
+            : `${text} no es un pangrama`,
+    );
+}
